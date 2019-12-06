a4ac80cc4142412d1327bee75c1f41677cb6f93a
Magenta49 <41891247+Magenta49@users.noreply.github.com>
2019-12-07 02:26:24 +0900

    Text

---
+++ Assets/Scenes/InGameScene/Script/PopupOnOff.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupOnOff : MonoBehaviour
+{
+    public GameObject quest;
+
+    public void QuestPopup()
+    {
+        if (!quest.gameObject.activeSelf)
+            quest.gameObject.SetActive(true);
+        else
+            quest.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scenes/InGameScene/Script/ValueSetting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValueSetting : MonoBehaviour
+{
+    public Text Gold;
+    public Text Crystal;
+    public Text Population;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Gold.text = "";
+        Crystal.text = "";
+        Population.text = "/";
+    }
+}
