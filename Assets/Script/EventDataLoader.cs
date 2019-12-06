3993373a8e4098177ff4dd5ab53df846008e9723
Jin-Hwan Kim <46664980+GouGit@users.noreply.github.com>
2019-12-07 01:02:42 +0900

    플레이어 기본 템플릿

---
+++ Assets/Script/EventDataLoader.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using LitJson;
+
+// user data 
+[SerializeField]
+public class GameData
+{
+	public static List<string> playerData = new List<string>();
+}
+
+public class EventDataLoader : MonoBehaviour
+{
+    // 큰스테이지, 세부스테이지
+	private int gamestagenum = 4, ingameStagenum = 3;
+
+	void Start()
+	{
+		// data not null
+		if (File.Exists(Application.persistentDataPath + "/GameData.json"))
+		{
+			LoadData();
+		}
+		// data null
+		else
+		{
+			
+			SaveData();
+		}
+	}
+
+	public void SaveData()
+	{
+		GameData tempData = new GameData();
+		
+		// data save to json
+		JsonData gamemData = JsonMapper.ToJson(tempData);
+
+		// create json file
+		File.WriteAllText(Application.persistentDataPath + "/GameData.json",gamemData.ToString());
+	}
+
+	public void LoadData()
+	{
+		// load json file
+		string JsonString = File.ReadAllText(Application.persistentDataPath + "/GameData.json");
+
+		// txt to object
+		JsonData gamemData = JsonMapper.ToObject(JsonString);
+
+		// load to array
+		
+	}
+
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerType
+{
+    public enum Player_Type
+    {
+        WARRIOR,
+        MAGICIAN,
+        ARCHER,
+        PRIEST
+    }    
+}
+
+[CreateAssetMenu(fileName = "Player Templete", menuName = "Players")]
+public class Player : ScriptableObject
+{
+    public Sprite image; //병사 이미지
+    public PlayerType.Player_Type type; //병사의 클래스
+    public new string name; //병사 이름 
+    public string rank; //병사 등급
+    public int hp; //병사 체력
+    public int power; //병사 전투력
+    public int level = 1; //병사 레벨 
+}
