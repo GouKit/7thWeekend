46cbd33a95ea464e01ea734e82e6cc3552eca22e
Magenta49 <41891247+Magenta49@users.noreply.github.com>
2019-12-08 03:29:29 +0900

    re
    retire

--- Assets/Script/EventManager.cs
+++ Assets/Script/EventManager.cs
@@ -6,10 +6,20 @@ public class EventManager : MonoBehaviour
 {
     private int maxQuest = 4;
     public List<GameObject> MonsterList = new List<GameObject>();
-    
+    public Transform questList;
+    public GameObject quest;
+
+    private void Start()
+    {
+        CreateQuest();
+    }
+
     public void CreateQuest()
     {
         //퀘스트 생성 ui 발생
+        GameObject em = Instantiate(quest, Vector3.zero, Quaternion.identity);
+        em.transform.SetParent(questList);
+        Debug.Log("asddsafasdfasfd");
     }
        
 }
---
+++ Assets/Scripts/ListExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ListExtension
+{
+	public static T GetRandomElement<T>(this List<T> list)
+	{
+		if (list.Count == 0)
+		{
+			return default;
+		}
+		var index = Random.Range(0, list.Count);
+		return list[index];
+	}
+}
