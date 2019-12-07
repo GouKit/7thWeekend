f497bde9e833d34cdd13dddde4ce54a8fc027d1e
Jin-Hwan Kim <46664980+GouGit@users.noreply.github.com>
2019-12-07 21:48:01 +0900

    mage

---
+++ Assets/Script/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using LitJson;
+ 
+[SerializeField]
+public class ItemSlot
+{
+	public static List<PlayerBase> playerData = new List<PlayerBase>();
+}
+
+public class Inventory : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+}
