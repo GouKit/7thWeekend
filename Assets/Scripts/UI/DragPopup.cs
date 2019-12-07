4c4e37b6b3c723dab26f5f2354d937dab93c7d88
sindy <sindydwns@naver.com>
2019-12-07 17:21:52 +0900

    commit

---
+++ Assets/Scripts/UI/DragPopup.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class DragPopup : MonoBehaviour
+{
+	public bool toggle;
+	public Dicrection dir;
+	private bool negative;
+	public float speed = 1;
+	private RectTransform rect;
+	private ReactiveProperty<float> now = new ReactiveProperty<float>();
+
+	protected void Awake()
+	{
+		rect = GetComponent<RectTransform>();
+		negative = dir == Dicrection.Right || dir == Dicrection.Up;
+		now.Value = negative ?
+			toggle ? 1 : 0 :
+			toggle ? 0 : 1;
+
+		if (dir == Dicrection.Up || dir == Dicrection.Down)
+		{
+			now.Subscribe(x => rect.pivot = new Vector2(0.5f, x)).AddTo(this);
+		}
+		else
+		{
+			now.Subscribe(x => rect.pivot = new Vector2(x, 0.5f)).AddTo(this);
+		}
+	}
+
+	protected void Update()
+	{
+		if (negative)
+		{
+			if (toggle)
+			{
+				var f = now.Value;
+				f += Time.deltaTime * speed;
+				f = f > 1 ? 1 : f;
+				now.Value = f;
+			}
+			else
+			{
+				var f = now.Value;
+				f -= Time.deltaTime * speed;
+				f = f < 0 ? 0 : f;
+				now.Value = f;
+			}
+		}
+		else
+		{
+			if (toggle)
+			{
+				var f = now.Value;
+				f -= Time.deltaTime * speed;
+				f = f < 0 ? 0 : f;
+				now.Value = f;
+			}
+			else
+			{
+				var f = now.Value;
+				f += Time.deltaTime * speed;
+				f = f > 1 ? 1 : f;
+				now.Value = f;
+			}
+		}
+	}
+
+	protected IEnumerator Cor(float delay, Action action)
+	{
+		yield return new WaitForSeconds(delay);
+		action();
+	}
+}
+
+public enum Dicrection
+{
+	Up,
+	Down,
+	Left,
+	Right
+}
---
+++ Assets/Scripts/UI/EventScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EnhancedUI.EnhancedScroller;
+
+public class EventScroller : MonoBehaviour, IEnhancedScrollerDelegate
+{
+    public EnhancedScrollerCellView GetCellView(EnhancedScroller scroller, int dataIndex, int cellIndex)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public float GetCellViewSize(EnhancedScroller scroller, int dataIndex)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public int GetNumberOfCells(EnhancedScroller scroller)
+    {
+        throw new System.NotImplementedException();
+    }
+}
