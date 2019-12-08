4e786fd95d14a32cc9c4767811aaacd7258cc082
Magenta49 <41891247+Magenta49@users.noreply.github.com>
2019-12-08 09:58:28 +0900

    몬스터

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
@@ -14,7 +12,7 @@ public class SceneChange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
+        if(Input.GetMouseButtonDown(0))
         {
             SceneManager.LoadScene("InGameScene");
         }
